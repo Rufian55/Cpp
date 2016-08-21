614d504f4ccc44c0022ab9e67b6fa5ba7d643c87
Chris Kearns <kearnsc@oregonstate.edu>
2016-08-21 16:38:44 -0400

    Update Patron.hpp

--- A Public Library/Patron.hpp
+++ A Public Library/Patron.hpp
@@ -10,14 +10,12 @@
 #include <vector>
 #include "Book.hpp"
 
-//class Library; is not needed!
-
 class Patron {
 private:
-	std::string idNum;					// Unique identifier for a Patron.
-	std::string name;					// Cannot assume to be unique.
+	std::string idNum;			// Unique identifier for a Patron.
+	std::string name;			// Cannot assume to be unique.
 	std::vector<Book*> checkedOutBooks;	// Vector of pointers to Books that Patron currently has checkedOut.
-	double fineAmount;					// How much the Patron owes the Library in late fines.
+	double fineAmount;			// How much the Patron owes the Library in late fines.
 
 public:
 	Patron(std::string idn, std::string n);
